856ff4d241a9a384dd3f09baff355adb77e89465
JEFFERSON <jeffersonnazariomendes@gmail.com>
2018-10-11 01:38:55 -0300

    Implementado o frame rate

--- src/SpaceInvadersClone/SpaceInvadersClone.java
+++ src/SpaceInvadersClone/SpaceInvadersClone.java
@@ -1,13 +1,20 @@
 package SpaceInvadersClone;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SpaceInvadersClone {
 
-    public SpaceInvadersClone() {
+    public SpaceInvadersClone() throws InterruptedException {
         new Tela();
     }
     
     public static void main(String[] args) {
-        new SpaceInvadersClone();
+        try {
+            new SpaceInvadersClone();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SpaceInvadersClone.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- src/SpaceInvadersClone/Tela.java
+++ src/SpaceInvadersClone/Tela.java
@@ -13,8 +13,18 @@ public class Tela extends JFrame {
     JPanel placar;
     JPanel campo;
     JPanel info;
-    
-    public Tela() {
+
+    final int QUADROS_POR_SEGUNDO = 60;
+    final double TAXA_ATUALIZACAO = 1000 / QUADROS_POR_SEGUNDO;
+    double mediaQuadros;
+    double tempoInicial;
+    double tempoExecucao;
+    double tempoTotal = 0;
+
+    int contFrame = 0;
+    int contFrameMaximo = 60;
+
+    public Tela() throws InterruptedException {
         this.setLayout(null);
         this.setSize(630, 800);
         this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getSize().width) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - this.getSize().height) / 2);
@@ -23,31 +33,42 @@ public class Tela extends JFrame {
         init();
         gameLoop();
     }
-    
+
     private void init() {
         placar = new Placar(this);
         campo = new Campo(this);
         info = new Info(this);
     }
-    
-    private void gameLoop() {
+
+    private void gameLoop() throws InterruptedException {
         while (true) {
+            tempoInicial = System.currentTimeMillis();
             update();
             render();
+            tempoExecucao = (double)System.currentTimeMillis() - tempoInicial;
+            Thread.sleep((long) Math.abs(TAXA_ATUALIZACAO - tempoExecucao));
+            tempoTotal += System.currentTimeMillis() - tempoInicial;
+            contFrame++;
+            if (contFrame == contFrameMaximo) {
+                mediaQuadros = 1000 / ((tempoTotal / contFrame));
+                contFrame = 0;
+                tempoTotal = 0;
+            }
         }
     }
-    
+
     private void update() {
         handleEvents();
     }
-    
+
     private void render() {
         this.add(placar);
         this.add(campo);
         this.add(info);
+        System.out.println("FPS " + mediaQuadros);
         this.setVisible(true);
     }
-    
+
     private void handleEvents() {
         this.addKeyListener(new KeyAdapter() {
             @Override
