eb0a9193d29d25d1425a24453a7aba4347c0fc45
JEFFERSON <jeffersonnazariomendes@gmail.com>
2018-10-12 18:45:05 -0300

    iniciando criação do personagem do jogador

--- src/SpaceInvadersClone/Jogador.java
+++ src/SpaceInvadersClone/Jogador.java
@@ -1,24 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SpaceInvadersClone;
 
-/**
- *
- * @author aluno
- */
 public class Jogador extends Personagem {
+
+    public Jogador(String imagem, int x, int y, int largura, int altura) {
+        super(imagem, x, y, largura, altura);
+    }
 
     @Override
     public void atirar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void mover() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
--- src/SpaceInvadersClone/Personagem.java
+++ src/SpaceInvadersClone/Personagem.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SpaceInvadersClone;
 
-/**
- *
- * @author aluno
- */
-public abstract class Personagem implements Interacoes {
+public abstract class Personagem extends Sprite implements Interacoes {
+    
+    public Personagem(String imagem, int x, int y, int largura, int altura) {
+        super(imagem, x, y, largura, altura);
+    }
     
 }
